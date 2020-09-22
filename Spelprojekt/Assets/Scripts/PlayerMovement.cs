0bed172c97767cc354123cdde21b1c6a028f9b2c
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-09-22 14:14:16 +0200

    Collsion in 2 directions works

--- Spelprojekt/Assets/Scripts/PlayerMovement.cs
+++ Spelprojekt/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     float myMaxSpeed = 10;
     float myAcceleration = 1;
-
+    float myDecceleration = 1;
+    float myDrag = 0.1f;
     int myInputDirectionX = 0;
     int myInputDirectionY = 0;
 
@@ -18,38 +19,121 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     KeyCode myMoveRightKey = KeyCode.D;
 
+    [SerializeField]
+    LayerMask layerMask;
     Vector3 myCurrentVelocity;
-    
+
+    JumpState myJumpState;
+    enum JumpState
+    {
+        none,
+        jumping,
+        falling
+    }
     void Start()
     {
         
     }
     void Update()
     {
-        
+        GetInputs();
     }
     void FixedUpdate()
     {
-        
+
+        DoPhysics();
     }
     void ApplyForce(Vector3 aTargetVelocity)
     {
-
+        myCurrentVelocity = myCurrentVelocity + aTargetVelocity;
         
     }
     void GetInputs()
     {
+        if (Input.GetKey(myMoveLeftKey) && Input.GetKey(myMoveRightKey))
+        {
+            myInputDirectionX = 0;
+        }
+        else if(Input.GetKey(myMoveLeftKey))
+        {
+            myInputDirectionX = -1;
+        }
+        else if(Input.GetKey(myMoveRightKey))
+        {
+            myInputDirectionX = 1;
+        }
+        else
+        {
+            myInputDirectionX = 0;
+        }
+        //-------------------
+        if (Input.GetKey(myJumpKey))
+        {
+            myInputDirectionY = 1;
+        }
+        else
+        {
+            myInputDirectionY = 0;
+        }
 
+    }
+    void Deccelerate()
+    {
+        myCurrentVelocity.x = Mathf.MoveTowards(myCurrentVelocity.x, 0, myDecceleration);
     }
     void CastBox()
     {
+        RaycastHit2D hitInfo = Physics2D.BoxCast(transform.position, transform.localScale , 0, (myCurrentVelocity * Time.fixedDeltaTime) - (transform.localScale / 2) * myInputDirectionX, layerMask);
+        
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.normal.x > 0 && myInputDirectionX == -1)
+            {
+                 ApplyForce(myCurrentVelocity.magnitude * new Vector3(hitInfo.normal.x, hitInfo.normal.y,0));
 
-    }
-    void PredictBox()
-    {
+            }
+            if (hitInfo.normal.x < 0 && myInputDirectionX == 1)
+            {
+                ApplyForce(myCurrentVelocity.magnitude * new Vector3(hitInfo.normal.x, hitInfo.normal.y, 0));
+
+            }
+
+        }
 
     }
     void DoPhysics()
+    {
+
+        if (myCurrentVelocity.magnitude < myMaxSpeed)
+        {
+            ApplyForce(new Vector3(myAcceleration * myInputDirectionX, 0, 0));
+        }
+        else
+        {
+            if (myCurrentVelocity.x > 0)
+            {
+                myCurrentVelocity.x -= myDrag;
+            }
+            else
+            {
+                myCurrentVelocity.x += myDrag;
+            }
+        }
+        if (myInputDirectionX == 0)
+        {
+            Deccelerate();
+        }
+        if (myInputDirectionY == 1)
+        {
+
+        }
+
+        CastBox();
+
+        transform.Translate(myCurrentVelocity * Time.fixedDeltaTime);
+    }
+    
+    void DoJump()
     {
 
     }
