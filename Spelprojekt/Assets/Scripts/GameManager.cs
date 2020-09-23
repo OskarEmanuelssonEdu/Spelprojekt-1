0c8f95fb09f378d17213ccc2963cebd3e0267ff3
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-09-23 14:24:52 +0200

    GameManager, ScoreManager
    har gjrort en counter som fungerar samt förberätt viss UI elemetns att fungera för LD

--- Spelprojekt/Assets/Scripts/GameManager.cs
+++ Spelprojekt/Assets/Scripts/GameManager.cs
@@ -1,36 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Countdown Before Game Begins")]
     [SerializeField]
     float myCountDownTime = 3;
     float myCountDownTimer = 0;
+   
+    [Header("Level Compelet Screen Settings")]
+    [SerializeField]
+    GameObject myLevelCompleteScreen;
+    [SerializeField]
+    GameObject myGameOverScreen;
 
     [SerializeField]
-    ScoreManager scoreManager;
+    TextMeshProUGUI myTotalTimeText;
+
+    [Header("Reference Settings")]
     [SerializeField]
-    Player player;
+    ScoreManager myScoreManager;
+    [SerializeField]
+    Player myPlayer;
+   
+
     void Update()
     {
         CountDownToStart();
-        
+       
+        if (myPlayer.myCurrentHealth <= 0)
+        {
+            GameOver();
+        }
     }
     void CountDownToStart()
     {
-        if (myCountDownTimer>= myCountDownTime)
+        if (myCountDownTimer >= myCountDownTime)
         {
             // Game has started and player can move :D
-
+            StartGame();
         }
         else
         {
             myCountDownTimer += Time.deltaTime;
         }
     }
+    void LevelComplete()
+    {
+        myLevelCompleteScreen.SetActive(true);
+        myTotalTimeText.text = myScoreManager.TotalTime.ToString("0.00");
+    }
+    void GameOver()
+    {
+        myGameOverScreen.SetActive(true);
+        myTotalTimeText.text = myScoreManager.TotalTime.ToString("0.00");
+
+    }
     public Vector3 PlayerPosition()
     {
-        return player.transform.position;
+        return myPlayer.transform.position;
+    }
+    void StartGame()
+    {
+        myScoreManager.StartCounter = true;
+
     }
 }
