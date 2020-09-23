1ca5e4ddcef0c415f174f9178fb045423705f528
jesper-lundgren <jesper.lundgren@studerande.thegameassembly.se>
2020-09-23 13:36:55 +0200

    +Lethal Object, *Interface
    Added Lethal Object and implemented an interface which LethalObject works with/against

---
+++ Spelprojekt/Assets/Scripts/LethalObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LethalObject : MonoBehaviour
+{
+    public string myTargetTag;
+    public float myDamage;
+
+    private void OnCollisionEnter(Collision aGameObject)
+    {
+        if (aGameObject.gameObject.CompareTag(myTargetTag))
+        {
+            aGameObject.gameObject.GetComponent<IDamageable>().TakeDamage(myDamage);
+        }
+    }
+}
