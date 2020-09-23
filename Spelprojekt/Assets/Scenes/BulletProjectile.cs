7e4c246291428c0d1bf8c10e768a2a709ba3e33e
denisebroste <denise.broste@studerande.thegameassembly.se>
2020-09-23 11:51:13 +0200

    Bullet Weapon prefabs

---
+++ Spelprojekt/Assets/Scenes/BulletProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    private float bulletSpeed = 100;
+    Vector3 previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(0f, 0f, bulletSpeed * Time.deltaTime);
+
+        RaycastHit[] hits = Physics.RaycastAll(new Ray(previousPos, (transform.position - previousPos).normalized), (transform.position - previousPos).magnitude);
+
+        previousPos = transform.position;
+
+        for ( int bulletIndex = 0; bulletIndex < hits.Length; bulletIndex++)
+        {
+            Debug.Log(hits[bulletIndex].collider.gameObject.name);
+        }
+    }
+
+}
---
+++ Spelprojekt/Assets/Scenes/WeaponScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    private float bulletSpeed = 10f;
+    public Transform Weapon;
+
+    void Start()
+    {
+        InvokeRepeating("Shoot", 0f, 1f);
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = BulletManager.SharedInstance.GetPooledObject();
+        if(bullet != null)
+        {
+            bullet.transform.position = Weapon.transform.position;
+            bullet.transform.rotation = Weapon.transform.rotation;
+            bullet.SetActive(true);
+        }
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
