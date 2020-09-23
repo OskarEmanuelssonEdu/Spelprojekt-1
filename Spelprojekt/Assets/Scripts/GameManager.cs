8775b17ee6073a152324629dbc87a88d585df69a
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-09-23 12:07:32 +0200

    Game Manager, Score Manager

---
+++ Spelprojekt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    float myCountDownTime = 3;
+    float myCountDownTimer = 0;
+
+    
+
+    void Update()
+    {
+        CountDownToStart();
+        
+    }
+    void CountDownToStart()
+    {
+        if (myCountDownTimer>= myCountDownTime)
+        {
+            // Game has started and player can move :D
+
+        }
+        else
+        {
+            myCountDownTimer += Time.deltaTime;
+        }
+    }
+}
--- Spelprojekt/Assets/Scripts/ScoreManager.cs
+++ Spelprojekt/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float myTotalTime = 0;
+    [SerializeField]
+    TextMeshProUGUI myScoreTextMesh;
+    
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+    float CountTime()
+    {
+        return myTotalTime = myTotalTime + Time.deltaTime;        
+    }
+    void UpdateTextMeshTotalTime ()
+    {
+        myScoreTextMesh.SetText(CountTime().ToString("2")); // To string definrar hur många decimaler jag vill ränka med, så i detta fallet blir det två decimaler
+    }
+
 }
