fc2dbcf7de851c38c32f5eddca8591629c5d76ab
jesper-lundgren <jesper.lundgren@studerande.thegameassembly.se>
2020-09-23 14:57:13 +0200

    *LethalObject (Custom Collision)
    Changed from unity OnCollisionEnter to custom rectangle collision

--- Spelprojekt/Assets/Scripts/LethalObject.cs
+++ Spelprojekt/Assets/Scripts/LethalObject.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class LethalObject : MonoBehaviour
 {
-    public string myTargetTag;
     public float myDamage;
 
-    private void OnCollisionEnter(Collision aGameObject)
+    [SerializeField]
+    Player myPlayer;
+
+    private void Update()
     {
-        if (aGameObject.gameObject.CompareTag(myTargetTag))
+        Vector3 RectangleOneScale = transform.localScale,
+                RectangleTwoScale = myPlayer.transform.localScale,
+                RectangleOnePosition = transform.position,
+                RectangleTwoPosition = myPlayer.transform.position;
+
+        float RectangleOneRightSide = RectangleOnePosition.x - RectangleOneScale.x * .5f,
+              RectangleOneLeftSide = RectangleOnePosition.x + RectangleOneScale.x * .5f,
+              RectangleOneBottomSide = RectangleOnePosition.y + RectangleOneScale.y * .5f,
+              RectangleOneTopSide = RectangleOnePosition.y - RectangleOneScale.y * .5f,
+              RectangleTwoRightSide = RectangleTwoPosition.x - RectangleTwoScale.x * .5f,
+              RectangleTwoLeftSide = RectangleTwoPosition.x + RectangleTwoScale.x * .5f,
+              RectangleTwoTopSide = RectangleTwoPosition.y + RectangleTwoScale.y * .5f,
+              RectangleTwoBottomSide = RectangleTwoPosition.y - RectangleTwoScale.y * .5f;
+
+        // Collision detection between two rectangles
+        if (RectangleOneRightSide < RectangleTwoLeftSide &&
+            RectangleOneLeftSide > RectangleTwoRightSide &&
+            RectangleOneTopSide > RectangleTwoBottomSide &&
+            RectangleOneBottomSide < RectangleTwoTopSide)
         {
-            aGameObject.gameObject.GetComponent<IDamageable>().TakeDamage(myDamage);
+            myPlayer.TakeDamage(myDamage * Time.deltaTime);
         }
+    }
+
+    [ExecuteInEditMode]
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, transform.localScale); 
     }
 }
