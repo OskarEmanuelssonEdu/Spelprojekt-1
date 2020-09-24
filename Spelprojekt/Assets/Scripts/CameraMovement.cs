c7f602db911ebca3da7684b407aecdbf2ea7621e
Oskar <oskar.emanuelsson@hotmail.se>
2020-09-24 15:24:43 +0200

    Revert "Merge branch 'CameraMovement'"
    This reverts commit e8176e614880f5a1e7215c4dcabda4d3fdf030ae, reversing
    changes made to 09564fbd1f9037c318228e79f2a85c9f78043ac1.

--- Spelprojekt/Assets/Scripts/CameraMovement.cs
+++ Spelprojekt/Assets/Scripts/CameraMovement.cs
@@ -6,108 +6,82 @@ using UnityEditor;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField]
-    Camera myCamera;
+    private float myMovementSpeed;
+    [SerializeField]
+    private float myMaxMovementSpeed;
+    private float myCurrentMovementSpeed;
     [SerializeField]
     private GameObject myPlayer;
 
-    [Header("SPEED Settings")]
-    [SerializeField]
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-    [Range(0f, 100f)]
-    private float myMinMovementSpeed;
-    [SerializeField]
-    [Range(0f, 100f)]
-=======
-    [Range(0f, 5f)]
-    private float myOverallSpeed;
-    [SerializeField]
-    [Range(0f, 100f)]
-    private float myMinMovementSpeed;
-    [SerializeField]
-    [Range(0f, 100f)]
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-    private float myMaxMovementSpeed;
-    [SerializeField]
-    [Range(0f, 100f)]
-    private float myCurrentMovementSpeed;
-    [SerializeField]
-    [Range(0f, 100f)]
-    private float myCameraFollowAcceleration;
-    [SerializeField]
-    [Range(0f, 100f)]
-    private float myCameraFollowDeceleration;
-
-
-
-    [Header("BOUNDARY Settings")]
-    [Header("All the way to the left is 0, to the right is 1")]
+    [Header("All the way to the left is 0, to the right is 100")]
     //How close to the edge of the screen can the player get before dying
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     private float myDistanceAllowedFromEdge;
     //How far to the right can the player run before the camera starts zooming out
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     private float myDistanceBeforeZoomingOut;
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     private float myDistanceBeforeSpeedingUp;
     [SerializeField]
-    [Range(0f, 1f)]
+    [Range(0f, 100f)]
     private float myDistanceBeforeSlowingDown;
 
 
     [Header("FOV Settings")]
     [SerializeField]
-    [Range(0f, 50f)]
     private float myZoomOutSpeed;
     [SerializeField]
-    [Range(0f, 50f)]
     private float myZoomInSpeed;
+    //If Camera is ortographic, change to size instead of FOV
     [SerializeField]
-    [Range(0f, 50f)]
+    [Range(0f, 150f)]
     private float myMinFieldOfView;
     [SerializeField]
-    [Range(0f, 50f)]
+    [Range(0f, 150f)]
     private float myMaxFieldOfView;
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-    
-=======
-
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
+    //How fast does move to the player when zooming out
+    [SerializeField]
+    [Range (0f, 10f)]
+    private float myCameraFollowZoomOutSpeed;
+    [SerializeField]
+    Camera myCamera;
 
 
     private void Start()
     {
-        myCurrentMovementSpeed = myMinMovementSpeed;
+        myCurrentMovementSpeed = myMovementSpeed;
     }
     void Update()
     {
+        Debug.Log("Camera Speed: " + myCurrentMovementSpeed);
         CheckPlayerScreenLocation();
         Move();
 
-        if (myCamera.orthographicSize > myMaxFieldOfView)
+        if (myCamera.fieldOfView > myMaxFieldOfView)
         {
-            myCamera.orthographicSize = myMaxFieldOfView;
+            myCamera.fieldOfView = myMaxFieldOfView;
         }
     }
+
+    
 
     private void CheckPlayerScreenLocation()
     {
         Vector3 screenPoint = myCamera.WorldToViewportPoint(myPlayer.transform.position);
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
         
-=======
+        bool isTouchingLeftBound = screenPoint.x < myDistanceAllowedFromEdge/100;
+        bool isTouchingZoomOutBound = screenPoint.x > myDistanceBeforeZoomingOut/100;
 
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-        bool isTouchingLeftBound = screenPoint.x < myDistanceAllowedFromEdge;
-        bool isTouchingZoomOutBound = screenPoint.x > myDistanceBeforeZoomingOut;
-        bool isTouchingSpeedUpBound = screenPoint.x > myDistanceBeforeSpeedingUp;
-        bool isTouchingSlowDownBound = screenPoint.x < myDistanceBeforeSlowingDown;
+        bool isTouchingSpeedUpBound = screenPoint.x > myDistanceBeforeSpeedingUp / 100;
+        bool isTouchingSlowDownBound = screenPoint.x < myDistanceBeforeSlowingDown / 100;
 
         if (isTouchingLeftBound)
         {
             //Kill Player
+            //Destroy(myPlayer);
             Debug.Log("Touching Left Bound");
         }
         if (isTouchingZoomOutBound)
@@ -115,76 +89,39 @@ public class CameraMovement : MonoBehaviour
             Debug.Log("Touching ZoomOut Bound");
             ZoomOut();
         }
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-        
-=======
-
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
+        if ((myCamera.fieldOfView > myMinFieldOfView))
+        {
+            ZoomIn();
+        }
         if (isTouchingSpeedUpBound)
         {
             Debug.Log("Touching SpeedUp Bound");
-            ZoomOut();
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-            myCurrentMovementSpeed += myCameraFollowAcceleration * Time.deltaTime * screenPoint.x;
-=======
-            myCurrentMovementSpeed += myCameraFollowAcceleration * Time.deltaTime * (screenPoint.x)* (screenPoint.x) * myOverallSpeed;
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-            if (myCurrentMovementSpeed > myMaxMovementSpeed)
-            {
-                myCurrentMovementSpeed = myMaxMovementSpeed;
-            }
+            myCurrentMovementSpeed = (int)Mathf.Lerp(myCurrentMovementSpeed, myMaxMovementSpeed, myCameraFollowZoomOutSpeed * Time.deltaTime);
+           
         }
-        if (isTouchingSlowDownBound)
+        if(isTouchingSlowDownBound)
         {
-            ZoomIn();
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-            myCurrentMovementSpeed -= myCameraFollowDeceleration * Time.deltaTime;
-=======
-            //Camera will deccelerate faster the closer the player gets to the edge of the screen
-            myCurrentMovementSpeed -= myCameraFollowDeceleration * Time.deltaTime * (1-screenPoint.x)* (1 - screenPoint.x) * myOverallSpeed;
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-            if (myCurrentMovementSpeed < myMinMovementSpeed)
-            {
-                myCurrentMovementSpeed = myMinMovementSpeed;
-            }
             Debug.Log("Touching SlowDown Bound");
+            myCurrentMovementSpeed = (int)Mathf.Lerp(myCurrentMovementSpeed, myMovementSpeed, myCameraFollowZoomOutSpeed * Time.deltaTime);
         }
+
+        
+
     }
     private void Move()
     {
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-        
         transform.Translate(new Vector3(myCurrentMovementSpeed * Time.deltaTime, 0, 0));
 
-    }
-    private void ZoomIn()
-    {
-        myCamera.orthographicSize -= myZoomInSpeed * Time.deltaTime;
-=======
-
-        transform.Translate(new Vector3(myCurrentMovementSpeed * Time.deltaTime * myOverallSpeed, 0, 0));
 
     }
     private void ZoomIn()
     {
-        myCamera.orthographicSize -= myZoomInSpeed * Time.deltaTime * myOverallSpeed;
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-        if (myCamera.orthographicSize < myMinFieldOfView)
-        {
-            myCamera.orthographicSize = myMinFieldOfView;
-        }
+        myCamera.fieldOfView -= myZoomInSpeed * Time.deltaTime;
     }
 
     private void ZoomOut()
     {
-<<<<<<< Updated upstream:Spelprojekt/Assets/CameraMovement.cs
-        myCamera.orthographicSize += myZoomOutSpeed * Time.deltaTime;
-=======
-        myCamera.orthographicSize += myZoomOutSpeed * Time.deltaTime * myOverallSpeed;
->>>>>>> Stashed changes:Spelprojekt/Assets/Scripts/CameraMovement.cs
-        if (myCamera.orthographicSize > myMaxFieldOfView)
-        {
-            myCamera.orthographicSize = myMaxFieldOfView;
-        }
+        myCamera.fieldOfView += myZoomOutSpeed * Time.deltaTime;
+        
     }
 }
