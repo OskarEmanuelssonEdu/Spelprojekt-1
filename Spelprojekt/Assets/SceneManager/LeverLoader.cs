df694c10a44b2ea46dcadff37e87b45059addbc1
veronikawang <veronikawang123@gmail.com>
2020-09-25 12:57:40 +0200

    SceneManager

---
+++ Spelprojekt/Assets/SceneManager/LeverLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeverLoader : MonoBehaviour
+{
+    public Animator myTransition;
+    
+    float myTransitionTime = 2f;
+    int myLevelIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+
+        myLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (myLevelIndex > 2)
+        {
+            myLevelIndex = 0;
+        }
+        StartCoroutine(LoadLevel(myLevelIndex));
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        myTransition.SetTrigger("Start");
+        yield return new WaitForSeconds(myTransitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
