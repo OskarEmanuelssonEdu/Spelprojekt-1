4cb198f5defa0bddf24130f86d93c5f458153dd2
veronikawang <veronikawang123@gmail.com>
2020-10-05 11:02:51 +0200

    Added CameraShaker

---
+++ Spelprojekt/Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public float shakeRadius = 1.0f;
+    public float duration = 1.0f;
+    public float slowDownAmonut = 1.0f;
+
+
+    public bool shouldShake = false;
+    public Transform camera;
+
+    Vector3 startPosition;
+    float initialDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main.transform;
+        startPosition = camera.localPosition;
+        initialDuration = duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldShake)
+        {
+            if (duration > 0)
+            {
+                camera.localPosition = startPosition + Random.insideUnitSphere * shakeRadius;
+                duration -= Time.deltaTime * slowDownAmonut;
+            }
+            else
+            {
+                shouldShake = false;
+                duration = initialDuration;
+                camera.localPosition = startPosition;
+            }
+        }
+    }
+}
