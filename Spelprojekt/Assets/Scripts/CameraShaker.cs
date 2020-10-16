99794e98c3c83b487b5f3a3e0957e42aea065499
Oskar <oskar.emanuelsson@hotmail.se>
2020-10-16 11:15:11 +0200

    Groundwork shake

--- Spelprojekt/Assets/Scripts/CameraShaker.cs
+++ Spelprojekt/Assets/Scripts/CameraShaker.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class CameraShaker : MonoBehaviour
 {
     public float shakeRadius = 1.0f;
-    public float duration = 1.0f;
+    float duration = 1.0f;
     public float slowDownAmonut = 1.0f;
-
+    [SerializeField]
+    float initialDuration;
+    [SerializeField]
+    NewCameraMovement myNewCameraMovement;
 
     public bool shouldShake = false;
     public Transform camera;
 
     Vector3 startPosition;
-    float initialDuration;
+
 
     // Start is called before the first frame update
+    private void OnValidate()
+    {
+
+        myNewCameraMovement = FindObjectOfType<NewCameraMovement>();
+
+    }
     void Start()
     {
         camera = Camera.main.transform;
@@ -26,6 +35,10 @@ public class CameraShaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        camera = Camera.main.transform;
+        startPosition = camera.localPosition;
+        print(startPosition);
+
         if (shouldShake)
         {
             if (duration > 0)
@@ -37,7 +50,7 @@ public class CameraShaker : MonoBehaviour
             {
                 shouldShake = false;
                 duration = initialDuration;
-                camera.localPosition = startPosition;
+                camera.localPosition = new Vector3(startPosition.x, startPosition.y, -15);
             }
         }
     }
