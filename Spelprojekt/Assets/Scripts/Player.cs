41ac663e1f280557f75a31a445f222936d1e82a2
Andre bruce <Andre.bruce@studerande.thegameassebly.se>
2020-10-22 11:26:18 +0200

    Fixed reset scirpt

--- Spelprojekt/Assets/Scripts/Player.cs
+++ Spelprojekt/Assets/Scripts/Player.cs
@@ -35,7 +35,7 @@ public class Player : MonoBehaviour
         
         if (myCurrentHealth <= 0)
         {
-            myGameManager.ResetGame();
+            myGameManager.ResetGame(false);
         
         }
     }
