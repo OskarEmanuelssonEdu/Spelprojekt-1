9bdef078c53c535591785367dffbfb62cadefbc2
Oskar <oskar.emanuelsson@hotmail.se>
2020-10-23 13:52:21 +0200

    Removed Debug Logs

--- Spelprojekt/Assets/Scripts/ObjectFalling.cs
+++ Spelprojekt/Assets/Scripts/ObjectFalling.cs
@@ -60,7 +60,7 @@ public class ObjectFalling : MonoBehaviour
             if (!isGrounded)
             {
                 velocity.y -= gravity * Time.fixedDeltaTime;
-                Debug.Log("Current velocity Y = " + velocity.y);
+                //Debug.Log("Current velocity Y = " + velocity.y);
                 transform.position += velocity * Time.fixedDeltaTime;
             }
 
