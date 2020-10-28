1923de494da41d073097a1915b9b19a0b090e9ce
Oskar <oskar.emanuelsson@hotmail.se>
2020-10-28 09:54:29 +0100

    Ui stuff

--- Spelprojekt/Assets/Scripts/LevelManager.cs
+++ Spelprojekt/Assets/Scripts/LevelManager.cs
@@ -152,6 +152,8 @@ public class LevelManager : MonoBehaviour
     {
         if (!myLevelIsGoingToReset)
         {
+            Unpause();
+
             myLevelIsGoingToReset = true;
 
             myPlayerMovement.enabled = false;
