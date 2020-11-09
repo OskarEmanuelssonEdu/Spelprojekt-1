a3982e7b2e217fd20dc3cbcc79f1a6eca7c4bc6e
veronikawang <veronikawang123@gmail.com>
2020-11-09 17:15:42 +0100

    added lines in script

--- Spelprojekt/Assets/Scripts/RatioManager.cs
+++ Spelprojekt/Assets/Scripts/RatioManager.cs
@@ -38,6 +38,12 @@ public class RatioManager : MonoBehaviour
 
     }
 
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
